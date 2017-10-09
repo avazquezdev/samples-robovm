cf78181ef83a72ea2416536ba35b2f3d3453c508
Alex <Nero88@MacBook-de-Alex.local>
2017-10-09 12:14:05 +0200

    Add NavigationController in MainView

--- HelloWorld/src/main/java/Main.java
+++ HelloWorld/src/main/java/Main.java
@@ -4,22 +4,29 @@ import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
 import org.robovm.apple.uikit.UIApplicationDelegateAdapter;
 import org.robovm.apple.uikit.UIApplicationLaunchOptions;
+import org.robovm.apple.uikit.UINavigationController;
 import org.robovm.apple.uikit.UIScreen;
 import org.robovm.apple.uikit.UIWindow;
 
 public class Main extends UIApplicationDelegateAdapter {
     private UIWindow window;
     private MyViewController rootViewController;
+    private UINavigationController navController;
+
 
     @Override
     public boolean didFinishLaunching(UIApplication application, UIApplicationLaunchOptions launchOptions) {
         // Set up the view controller.
         rootViewController = new MyViewController();
 
+        navController = new UINavigationController(rootViewController);
+        navController.getNavigationBar().setHidden(false);
+        navController.setTitle("Hello World");
+        
         // Create a new window at screen size.
         window = new UIWindow(UIScreen.getMainScreen().getBounds());
         // Set the view controller as the root controller for the window.
-        window.setRootViewController(rootViewController);
+        window.setRootViewController(navController);
         // Make the window visible.
         window.makeKeyAndVisible();
 
