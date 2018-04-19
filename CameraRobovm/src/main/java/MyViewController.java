e859e0ee2ef361f044d600f9b1aa1be17a5b9c14
alex <vazlup@gmail.com>
2018-04-19 11:51:00 +0200

    Add decode Qr codes

--- CameraRobovm/src/main/java/MyViewController.java
+++ CameraRobovm/src/main/java/MyViewController.java
@@ -1,5 +1,7 @@
 
+import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.Hashtable;
 
 import org.robovm.apple.avfoundation.AVAuthorizationStatus;
 import org.robovm.apple.avfoundation.AVCaptureConnection;
@@ -11,6 +13,7 @@ import org.robovm.apple.avfoundation.AVCaptureMetadataOutputObjectsDelegate;
 import org.robovm.apple.avfoundation.AVCaptureMetadataOutputObjectsDelegateAdapter;
 import org.robovm.apple.avfoundation.AVCaptureOutput;
 import org.robovm.apple.avfoundation.AVCaptureSession;
+import org.robovm.apple.avfoundation.AVCaptureSessionPreset;
 import org.robovm.apple.avfoundation.AVCaptureVideoDataOutput;
 import org.robovm.apple.avfoundation.AVCaptureVideoDataOutputSampleBufferDelegateAdapter;
 import org.robovm.apple.avfoundation.AVCaptureVideoOrientation;
@@ -20,6 +23,7 @@ import org.robovm.apple.avfoundation.AVMediaType;
 import org.robovm.apple.avfoundation.AVMetadataObject;
 import org.robovm.apple.avfoundation.AVMetadataObjectType;
 import org.robovm.apple.avfoundation.AVPixelBufferAttributes;
+import org.robovm.apple.coregraphics.CGBitmapContext;
 import org.robovm.apple.coregraphics.CGDataProvider;
 import org.robovm.apple.coregraphics.CGImage;
 import org.robovm.apple.coregraphics.CGRect;
@@ -27,6 +31,7 @@ import org.robovm.apple.coregraphics.CGSize;
 import org.robovm.apple.coreimage.CIContext;
 import org.robovm.apple.coreimage.CIImage;
 import org.robovm.apple.coremedia.CMSampleBuffer;
+import org.robovm.apple.corevideo.CVImageBuffer;
 import org.robovm.apple.corevideo.CVPixelBuffer;
 import org.robovm.apple.corevideo.CVPixelFormatType;
 import org.robovm.apple.dispatch.DispatchQueue;
@@ -48,6 +53,16 @@ import org.robovm.apple.uikit.UIInterfaceOrientation;
 import org.robovm.apple.uikit.UILabel;
 import org.robovm.apple.uikit.UIView;
 import org.robovm.apple.uikit.UIViewController;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.PlanarYUVLuminanceSource;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
 
 public class MyViewController extends UIViewController {
 
@@ -96,24 +111,25 @@ public class MyViewController extends UIViewController {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		else
+		} else
 			Foundation.log("No cameras.");
-			
-
 	}
 
 	public class CameraMobileViewAdapterImpl {
 
 		private final UIView view;
-
+		private final UILabel resultLabel;
 		private AVCaptureVideoPreviewLayer previewLayer;
 		private AVCaptureSession session;
 		private AVCaptureDevice cameraDevice;
+		private CaptureMetadataOutputDelegate captureDelegate;
+		private AVCaptureMetadataOutput captureMetadataOutput;
+
 
 		public CameraMobileViewAdapterImpl(UIView view) {
 			this.view = view;
-		}
+			resultLabel = new UILabel(new CGRect(0, 0, 500, 80));
+			resultLabel.setText("Test");		}
 
 		private boolean initializeCameraDevice() {
 			cameraDevice = AVCaptureDevice.getDefaultDeviceForMediaType(AVMediaType.Video);
@@ -167,18 +183,28 @@ public class MyViewController extends UIViewController {
 
 			AVCaptureVideoDataOutput videoDataOutput = new AVCaptureVideoDataOutput();
 
+			
 			DispatchQueue queue = DispatchQueue.create("cameraQueue", DispatchQueueAttr.Serial());
 			videoDataOutput.setSampleBufferDelegate(new BufferDelegate(), queue);
 
+			captureMetadataOutput = new AVCaptureMetadataOutput();
+			captureDelegate = new CaptureMetadataOutputDelegate(resultLabel);
+	        captureMetadataOutput.setMetadataObjectsDelegate(captureDelegate, queue);
+
+	        
+
 			AVPixelBufferAttributes attributes = new AVPixelBufferAttributes();
-			attributes.setPixelFormatType(CVPixelFormatType._32BGRA);
+			attributes.setPixelFormatType(CVPixelFormatType._32BGRA);// _32BGRA,_32ABGR
 			videoDataOutput.setPixelBufferSettings(attributes);
 
 			session = new AVCaptureSession();
 			session.addOutput(videoDataOutput);
 			session.addInput(input);
-			// session.setSessionPreset(AVCaptureSessionPreset.Size640x480);
+			session.setSessionPreset(AVCaptureSessionPreset.Size640x480);
 
+			if(session.canAddOutput(captureMetadataOutput)) session.addOutput(captureMetadataOutput);
+	        captureMetadataOutput.setMetadataObjectTypes(Arrays.asList(AVMetadataObjectType.QRCode));
+	        
 			previewLayer = new AVCaptureVideoPreviewLayer(session);
 			previewLayer.setVideoGravity(AVLayerVideoGravity.ResizeAspectFill);
 
@@ -189,7 +215,8 @@ public class MyViewController extends UIViewController {
 			connection.setVideoOrientation(AVCaptureVideoOrientation.LandscapeRight);
 
 			view.getLayer().addSublayer(previewLayer);
-
+			resultLabel.setText("Alog");
+			view.getLayer().addSublayer(resultLabel.getLayer());
 			session.startRunning();
 
 		}
@@ -203,24 +230,70 @@ public class MyViewController extends UIViewController {
 			private CIContext context;
 			private CGImage image;
 			private byte[] bytes;
-			
+
 			@Override
 			public void didOutputSampleBuffer(AVCaptureOutput output, CMSampleBuffer mBuffer,
 					AVCaptureConnection connection) {
 
-				Foundation.log("SampleBuffer"); 
+				// Foundation.log("SampleBuffer");
 
 				try {
 
 					CVPixelBuffer buffer = mBuffer.getPixelBuffer();
+					// Get image buffer and lock base address
+					CVImageBuffer imageBuffer = mBuffer.getImageBuffer();// CMSampleBufferGetImageBuffer(mBuffer);
+					// imageBuffer.g//CVPixelBufferLockBaseAddress(imageBuffer, 0);
+
+					// Get image buffer informations
+					// final int baseAddress = (int)
+					// imageBuffer.;//VPixelBufferGetBaseAddress(imageBuffer);
+					final long width = getUnsignedLong(buffer.getWidth());// CVPixelBufferGetWidth(imageBuffer));
+					final long height = getUnsignedLong(buffer.getHeight());// CVPixelBufferGetHeight(imageBuffer));
+					final long bytesPerRow = getUnsignedLong(buffer.getBytesPerRow());// CVPixelBufferGetBytesPerRow(imageBuffer));
+					final long bytesPerPixel = bytesPerRow / width;
+					final long size = width * height * bytesPerPixel;
 
 					ciImage = new CIImage(buffer);
 					context = new CIContext();
 
-					image = context.createCGImage(ciImage,
-							new CGRect(0, 0, buffer.getWidth(), buffer.getHeight()));
+					image = context.createCGImage(ciImage, new CGRect(0, 0, buffer.getWidth(), buffer.getHeight()));
 
 					bytes = image.getDataProvider().getData().getBytes();
+
+					/*long[] arry = buffer.getExtendedPixels();
+					for (int i = 0; i < arry.length; ++i)
+						pixels[i] = (int) arry[i];
+
+					int maxY = height;
+					for (int y = 0; y < maxY; y++) {
+						for (int x = 0; x < width; x++) {
+							int offset = bytesPerPixel * ((width * y) + x);
+							pixels2[offset] = pixels[offset + 2]; // R
+							pixels2[offset + 1] = pixels[offset + 1]; // G
+							pixels2[offset + 2] = pixels[offset]; // B
+							pixels2[offset + 3] = pixels[offset + 3]; // A
+						}
+					}
+					RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);*/
+
+					 LuminanceSource source = new PlanarYUVLuminanceSource(bytes, (int)
+					 image.getWidth(),
+					 (int) image.getHeight(), 0, 0, (int) image.getWidth(), (int)
+					 image.getHeight(), false);
+					BinaryBitmap bbitmap = new BinaryBitmap(new HybridBinarizer(source));
+					Result result;
+					Hashtable<DecodeHintType, Object> decodeHints = new Hashtable<>();
+					decodeHints.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+					decodeHints.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
+
+					try {
+						result = new MultiFormatReader().decodeWithState(bbitmap);
+						//Foundation.log(result.getText());
+						//resultLabel.setText(result.getText());
+					} catch (NotFoundException e) {
+						//Foundation.log("Unable to Decode");
+						//resultLabel.setText("Unable to Decode");
+					}
 
 				} catch (Throwable e) {
 					e.printStackTrace();
@@ -229,7 +302,37 @@ public class MyViewController extends UIViewController {
 
 			@Override
 			public void didDropSampleBuffer(AVCaptureOutput arg0, CMSampleBuffer arg1, AVCaptureConnection arg2) {
-				System.out.println("drop"); //$NON-NLS-1$
+				// System.out.println("drop"); //$NON-NLS-1$
+			}
+		}
+	}
+
+	private static long getUnsignedLong(long x) {
+		return x & 0xffffffffL;
+	}
+
+	private class CaptureMetadataOutputDelegate extends AVCaptureMetadataOutputObjectsDelegateAdapter {
+		//private IOSQRCodeScanner qrCodeScanner;
+		private final UILabel label;
+		public CaptureMetadataOutputDelegate(UILabel label) {
+		//	this.qrCodeScanner = IOSQRCodeScanner.this;
+			this.label= label;
+		}
+
+		@Override
+		public void didOutputMetadataObjects(AVCaptureOutput captureOutput, NSArray<AVMetadataObject> metadataObjects,
+				AVCaptureConnection connection) {
+			if (metadataObjects != null && metadataObjects.size() > 0) {
+				AVMetadataObject data = metadataObjects.get(0);
+				//data.getKeyValueCoder().getValues("stringValue");
+				if (data.getType() == AVMetadataObjectType.QRCode) {
+					// Found QR Code TODO - change data.toString()
+					//qrCodeScanner.onCodeScanned(data.toString());
+					Foundation.log("Code: " + data.getKeyValueCoder().getValues("stringValue").toString());
+					label.setText(data.getKeyValueCoder().getValues("stringValue").toString());
+				}
+
+				metadataObjects.dispose();
 			}
 		}
 	}
